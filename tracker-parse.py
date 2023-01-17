d018e345b369dce814c15f8003434845dde8036f
whiskishivers <117548751+whiskishivers@users.noreply.github.com>
2023-01-16 21:26:18 -1000

    Add files via upload

--- tracker-parse.py
+++ tracker-parse.py
@@ -3,30 +3,28 @@
 import html
 import os
 import re
+from urllib.parse import unquote
 
 # Load all text files in pwd and parse them for torrent tracker-like urls. Output domain list.
 
-tracker_rx = re.compile(r"([A-Za-z]+://)([-\w]+(?:\.\w[-\w]*)+)(:\d+)?/announce")
+tracker_rx = re.compile(r"([A-Za-z]+://)([-\w]+(?:\.\w[-\w]*)+)(:\d+)?/announce\W")
 data = ""
 files = [i for i in os.listdir() if i.endswith('.txt')]
 urls = set()
 domains = set()
 
 
-# Load data
 for i in files:
 	with open(i, 'r') as f:
-		data += html.unescape(f.read())
+		data += unquote(html.unescape(f.read()))
 
-# Parse trackers
 for i in tracker_rx.finditer(data):
 	urls.add(i[0])
 	domains.add(i[2])
 
-# Write to full url file
+print('\n'.join(sorted(list(domains))))
+
+# Save full urls
 with open('parsed-urls.txt', 'w') as f:
 	f.write('\n'.join(sorted(list(urls))))
-
-# Output unique tracker domains
-print('\n'.join(sorted(list(domains))))
 
